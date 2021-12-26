1aaf36e1b425584f4ec459eb151e8273ab4b4418
Admin <Admin@DESKTOP-PIE6SF7>
2021-12-26 12:03:13 -0800

    test ng commit

---
+++ src/TestNGDemoOne/NewTest.java
@@ -0,0 +1,17 @@
+package TestNGDemoOne;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	@Test  
+    public void test1()  
+    {  
+        System.out.println("Hello javaTpoint!!");  
+    }  
+      
+    @Test  
+    public void test2()  
+    {  
+        System.out.println("JTP Travels");
+ }
+}
---
+++ src/TestNGDemoOne/NewTest2.java
@@ -0,0 +1,11 @@
+package TestNGDemoOne;
+
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+	  @Test  
+	  public void test3()  
+	  {  
+	      System.out.println("hindi100.com");  
+	  }  
+}
